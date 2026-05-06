32d2d500b62e36a53175a1656e84a4a0b35d0439
Nestor Humberto Sanchez Vicente <nsanchezv23_1@unc.edu.pe>
2026-05-05 23:01:06 -0500

    primer commit: proyecto Sistema_almacen_DEPSA

---
+++ src/Producto.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author NESTOR
+ */
+public class Producto 
+{
+    // primera rama en el producto 
+    Double precio;
+    String Nombre;
+    int Codigo;
+    
+}
+// cambio hecho por Beeker
---
+++ src/Sistema_almacen_DEPSA.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+
+/**
+ *
+ * @author NESTOR
+ */
+public class Sistema_almacen_DEPSA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
