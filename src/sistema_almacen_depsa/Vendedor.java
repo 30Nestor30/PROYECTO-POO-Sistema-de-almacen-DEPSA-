4218ee6776f5bc7bc23af88980f99f782074915a
Beeker <jambertorres0@gmail.com>
2026-06-14 01:47:56 -0500

    Codigo de Vendedor Agregado

--- src/sistema_almacen_depsa/Vendedor.java
+++ src/sistema_almacen_depsa/Vendedor.java
@@ -8,6 +8,38 @@ package sistema_almacen_depsa;
  *
  * @author HP
  */
-public class Vendedor {
-    
+// === HERENCIA ===
+// Al igual que el Cliente, el Vendedor hereda los datos basicos de Persona.
+public class Vendedor extends Persona 
+{
+    private String codigoVendedor;
+    private String email;
+
+    // Constructor actualizado con los 7 parametros de Persona + los 2 del vendedor
+    public Vendedor(String tipo_documento, String documento, String nombres, String apellido_paterno, String apellido_materno, String direccion, String telefono, String codigoVendedor, String email) 
+    {
+        super(tipo_documento, documento, nombres, apellido_paterno, apellido_materno, direccion, telefono);
+        this.codigoVendedor = codigoVendedor;
+        this.email = email;
+    }
+
+    public String getCodigoVendedor() 
+    {
+        return codigoVendedor;
+    }
+
+    public void setCodigoVendedor(String codigoVendedor) 
+    {
+        this.codigoVendedor = codigoVendedor;
+    }
+
+    public String getEmail() 
+    {
+        return email;
+    }
+
+    public void setEmail(String email) 
+    {
+        this.email = email;
+    }
 }
