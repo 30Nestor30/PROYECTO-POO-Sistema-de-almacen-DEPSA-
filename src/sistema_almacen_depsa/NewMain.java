1b992bc1d0fb4291ea5a4e02dcb38a2ba859c9ac
Nestor Humberto Sanchez Vicente <nsanchezv23_1@unc.edu.pe>
2026-06-14 00:07:30 -0500

    Subida inicial: Main limpio para arranque

---
+++ src/sistema_almacen_depsa/NewMain.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sistema_almacen_depsa;
+
+/**
+ *
+ * @author NESTOR
+ */
+public class NewMain {
+
+
+   
+    public static void main(String[] args) {
+        System.out.println("=== INICIANDO SISTEMA DEPSA ===");
+        
+        // La prueba de Productos se ha retirado temporalmente 
+        // a la espera de que el equipo suba los modelos lógicos.
+        
+        // NOTA: Aquí agregaremos luego el código para hacer visible tu Ventana Principal
+    }
+
+}
